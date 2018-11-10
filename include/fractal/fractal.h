7e87c76bd142efa2130ad573e8286e9165f5f79e
dorodnic <dorodnic@gmail.com>
2018-11-10 13:43:04 +0200

    Adding OpenMP and bulk calls

--- include/fractal/fractal.h
+++ include/fractal/fractal.h
@@ -13,7 +13,17 @@ extern "C" {
 * \return                       Number of iterations until the point escapes
                                 from the local neighborhood of zero
 */
-int fractal_mandelbrot(float x, float y, int max_iterations);
+int fractal_mandelbrot(double x, double y, int max_iterations);
+
+/* 
+* Fills the escape times in iterations of points (x..,y..) from the mandlebrot set
+* \param[in] x                  X coordinates of the starting point
+* \param[in] y                  Y coordinates of the starting point
+* \param[in] count              Number of input points
+* \param[in] results            Pointer to the array where to store escape times
+* \param[in] max_iterations     Maximum number of iterations to perform
+*/
+void fractal_mandelbrot_bulk(double* x, double* y, int count, int* results, int max_iterations);
 
 #ifdef __cplusplus
 }
--- src/fractal.cpp
+++ src/fractal.cpp
@@ -2,12 +2,12 @@
 
 #include <complex>
 
-int fractal_mandelbrot(float x, float y, int max_iterations)
+int fractal_mandelbrot(double x, double y, int max_iterations)
 {
     using namespace std;
 
-    complex<float> point(x, y);
-    complex<float> z(0, 0);
+    complex<double> point(x, y);
+    complex<double> z(0, 0);
 
     int iteration = 0;
     while (abs (z) < 2 && iteration <= max_iterations) 
@@ -16,4 +16,13 @@ int fractal_mandelbrot(float x, float y, int max_iterations)
         iteration++;
     }
     return iteration;
+}
+
+void fractal_mandelbrot_bulk(double* x, double* y, int count, int* results, int max_iterations)
+{
+	#pragma omp parallel for
+	for (int i = 0; i < count; i++)
+	{
+		results[i] = fractal_mandelbrot(x[i], y[i], max_iterations);
+	}
 }
