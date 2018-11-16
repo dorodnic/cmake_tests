3eb5180441b5da72f8d464e9def6644e39fe54fb
Sergey Dorodnicov <dorodnic@gmail.com>
2018-11-16 16:41:05 +0100

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -26,10 +26,10 @@ TEST_CASE("Test bulk", "[mandelbrot]") {
         ys[i] = unif(re);
     }
     
-    //fractal_mandelbrot_bulk(xs, ys, MAX, res, iterations);
+    fractal_mandelbrot_bulk(xs, ys, MAX, res, iterations);
     for (int i = 0; i < MAX; i++)
     {
-        //REQUIRE(res[i] == fractal_mandelbrot(xs[i], ys[i], iterations));
+        REQUIRE(res[i] == fractal_mandelbrot(xs[i], ys[i], iterations));
     }
 }
 
